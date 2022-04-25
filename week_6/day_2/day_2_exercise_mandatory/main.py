19323eebfcc9bc2fdc277141c7e64748d5ad9270
Emmanuelle <berrebi_emmanuelle@yahoo.fr>
2022-04-26 00:42:05 +0200

    week_6 day_2 exercise_mandatory

---
+++ week_6/day_2/day_2_exercise_mandatory/main.py
@@ -0,0 +1,33 @@
+# Exercises XP
+
+# Exercise 1
+print('Hello world\n'*4)
+
+# Exercise 2
+print((99**3)*8)
+
+# Exercise 3
+5<3 #False
+3 == 3 #True
+3 == "3" #False
+#"3" > 3 #Error
+"Hello" == "hello" #False
+
+# Exercise 4
+computer_brand = 'Dell'
+print(f"I have a {computer_brand} computer")
+
+# Exercise 5
+"""Instructions
+1. Create a variable called name, and set it’s value to your name.
+2. Create a variable called age, and set it’s value to your age.
+3. Create a variable called shoe_size, and set it’s value to your shoe size.
+4. Create a variable called info and set it’s value to an interesting sentence about yourself. The sentence must contain all the variables created in parts 1, 2 and 3.
+5. Have your code print the info message.
+Run your code"""
+
+name = "Emmanuelle"
+age = 56
+shoe_size = 39
+info = f"Although I am already {age}, I undersigned {name} am fully unable to imagine any interesting sentence with my shoe size which is by the way {shoe_size} - this precision just in the improbable case it might interest someone "
+print(info)
