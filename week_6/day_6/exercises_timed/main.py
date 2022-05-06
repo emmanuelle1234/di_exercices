8dea499b7003618b34c9e7c452265020cb37cc27
Emmanuelle <berrebi_emmanuelle@yahoo.fr>
2022-05-06 17:31:54 +0200

    with week_6 day_6 timed_exercise_2

--- week_6/day_6/exercises_timed/main.py
+++ week_6/day_6/exercises_timed/main.py
@@ -10,3 +10,11 @@ if my_sum == x:
     print('True')
 else:
     print('False')
+
+
+#Timed exercise 2: write a program to reverse the sentence wordwise.
+sentence=input('Please enter a sentence: ')
+words=sentence.split(' ')
+words_reverse= reversed(words)
+sentence_reverse= ' '.join(words_reverse)
+print(sentence_reverse)
