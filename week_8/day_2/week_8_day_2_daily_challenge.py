1a1e7d6dd433d90cf483ef98436d6b2af8f89d7b
Emmanuelle <berrebi_emmanuelle@yahoo.fr>
2022-05-17 15:29:49 +0200

    week_8 day_2 daily_challenge

--- week_8/day_2/week_8_day_2_daily_challenge.py
+++ week_8/day_2/week_8_day_2_daily_challenge.py
@@ -5,26 +5,22 @@ class Farm:
     def __init__(self, name):
         self.name = name
         self.animals = {}
-        self.animals['cow']= 2
-        print(self.animals)
 
-
-
-    def add_animal(self, animal, number = 1):
-        #self.animals[animal] += number
-        for key, value in self.animals.items():
-            if animal != key:
-                self.animals[animal].append(number)
-            elif animal == key:
-                self.animals[animal] += number
-        print(self.animals)
-
+    def add_animal(self, animal, number=1):
+        item = {animal: number}
+        if animal not in self.animals.keys():
+            self.animals.update(item)
+        else:
+            self.animals[animal] += number
 
     def get_info(self):
         print(f'{self.name}\' farm')
+        print('')
         for key, value in self.animals.items():
             print(f'{key} : {value}')
-        print('E-I-E-I-O')
+        print('')
+        print('   E-I-E-I-O!')
+        return ""
 
 
 macdonald = Farm("McDonald")
@@ -33,7 +29,3 @@ macdonald.add_animal('sheep')
 macdonald.add_animal('sheep')
 macdonald.add_animal('goat', 12)
 print(macdonald.get_info())
-
-
-
-
