f8a875ffbe7e8c654a8100d1dff9d53e6192eed5
Ayal Ganem <ayalganem55@gmail.com>
2022-06-09 20:49:41 +0300

    Update anagram_checker.py

--- week_10/day_5/Miniproject1/anagram_checker.py
+++ week_10/day_5/Miniproject1/anagram_checker.py
@@ -5,22 +5,24 @@ class AnagramChecker:
 
     def __init__(self):
         with open('text.txt', 'r') as words_file:
-            self.words_list = []
+            self.words_list = [] # you define it twice, please remove
             self.words_list = [word.replace('\n', '') for word in words_file.readlines()]
 
     def is_english_word(self, word):
-        if word.upper() in self.words_list:
+        if word.upper() in self.words_list: # just replace with return word.upper() in self.words_list
             return True
         else:
             return False
 
     def is_anagram(self, word1, word2):
+        # the same replace with one line
         if sorted(word1) == sorted(word2):  # alternative solution to be thought with permutations in itertools?
             return True
         else:
             return False
 
     def get_anagrams(self, word):
+        # the var is redandant
         anagrams = [item.lower() for item in self.words_list if self.is_anagram(item, word.upper()) and item != word.upper()]
         return anagrams
 
